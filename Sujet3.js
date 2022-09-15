18fe42f1f3349d0270eb21f1d07baa573af6b959
Hugo Demont <hugodemont62@hotmail.fr>
2022-09-15 10:01:55 +0200

    Init

---
+++ Sujet3.js
@@ -0,0 +1,10 @@
+let car = {
+    "make" : "Tesla",
+    "model": "Model S",
+    "gears": ["P", "N", "R", "D"],
+    "year": 2020
+}
+
+for (let i = 0; i< car.gears.length;i++){
+    console.log(car.gears[i]);
+}
