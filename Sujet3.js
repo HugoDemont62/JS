5a23f6854e7f325fe869cc6056da7e5def6ce162
Hugo Demont <hugodemont62@hotmail.fr>
2022-09-15 11:07:29 +0200

    Init

--- Sujet3.js
+++ Sujet3.js
@@ -1,10 +1,32 @@
-let car = {
-    "make" : "Tesla",
-    "model": "Model S",
-    "gears": ["P", "N", "R", "D"],
-    "year": 2020
-}
+//let car = {
+//    "make": "Tesla",
+//    "model": "Model S",
+//    "gears": ["P", "N", "R", "D"],
+//    "year": 2020
+//}
+//for (let i = 0; i < car.gears.length; i++) {
+//    console.log(car.gears[i]);
+//}
+//let {critique} = {
+//    "name": "QOAK",
+//    "realisateur": "Maxime Richard",
+//    "acteur": ["H Demont", "M Richard", "K Guerin", "R Leblond", "F Throraval", "E Grand", "G Mirebel"],
+//    "critique": [2, 4, 5, 4],
+//}
+//function moyenne(tab) {
+//    let somme = 0;
+//    for (let i = 0; i < tab.length; i++) {
+//        somme += tab[i];
+//    }
+//    somme = somme / tab.length;
+//    return somme;
+//}
+//console.log(moyenne(critique));
+//
+let jsonFile = '{"name":"John","age":30, "city":"New York"}';
 
-for (let i = 0; i< car.gears.length;i++){
-    console.log(car.gears[i]);
-}
+let jsonDecrypt = JSON.parse(jsonFile);
+console.log(jsonDecrypt.name);
+jsonDecrypt.age += 1;
+
+console.log(JSON.stringify(jsonDecrypt));
