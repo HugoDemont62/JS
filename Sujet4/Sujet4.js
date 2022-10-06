f49e503f25c780e3a784cde829485f6b1175c16b
HugoDemont <hugodemont62@hotmail.fr>
2022-10-06 08:49:12 +0200

    Init

--- Sujet4/Sujet4.js
+++ Sujet4/Sujet4.js
@@ -5,7 +5,7 @@ let etudiant1 = {
     "web": [15, 12, 10, 9, 15],
     "com": [11, 11, 13, 10, 10],
     "audiovisuel": [16, 13, 14, 13, 14]
-};
+}
 let etudiant2 = {
     "nom": "Sido",
     "prenom": "Corentin",
@@ -13,26 +13,34 @@ let etudiant2 = {
     "web": [9, 3, 20, 16, 15],
     "com": [18, 16, 13, 16, 15],
     "audiovisuel": [12, 13, 11, 16, 10]
-};
+}
 
 function buildCard(etudiant) {
     let div = document.createElement("div");
-    const {prenom, nom, age, web, com, audiovisuel} = etudiant;
-    div.append(`${prenom} ${nom} (${age})`);
-    const ul =document.createElement('ul');
-    const createLi = () => document.createElement('li');
-    const li = createLi();
-    const li2 = createLi();
-    const li3 = createLi();
-    li.append(web);
-    li2.append(com);
-    li3.append(audiovisuel);
-    ul.append(li, li2, li3);
-    div.append(ul);
+    const {prenom, nom, age, web, com, audiovisuel} = etudiant
+    div.append(`${prenom} ${nom} (${age})`)
+    const ul =document.createElement('ul')
+    const createLi = () => document.createElement('li')
+    const li = createLi()
+    const li2 = createLi()
+    const li3 = createLi()
+    li.append(web)
+    li2.append(com)
+    li3.append(audiovisuel)
+    ul.append(li, li2, li3)
+    div.append(ul)
     return div;
 }
-let d = buildCard(etudiant1)
-document.getElementById('root').append(d);
 
-let d2 = buildCard(etudiant2)
-document.getElementById('root').append(d2);
+let etudiants = [etudiant1, etudiant2];
+for (let etudiant of etudiants) {
+    let d = buildCard(etudiant)
+    document.getElementById('root').append(d);
+}
+
+let b = document.querySelector('button');
+b.onclick = () => {
+    let divs = document.querySelectorAll("#root div")
+    for (let div of divs)
+        div.classList.toggle("hidden");
+}
--- Sujet5/main.js
+++ Sujet5/main.js
@@ -1,46 +1,16 @@
-let etudiant1 = {
-    "nom": "Guerin",
-    "prenom": "Killian",
-    "age": 19,
-    "web": [15, 12, 10, 9, 15],
-    "com": [11, 11, 13, 10, 10],
-    "audiovisuel": [16, 13, 14, 13, 14]
-}
-let etudiant2 = {
-    "nom": "Sido",
-    "prenom": "Corentin",
-    "age": 20,
-    "web": [9, 3, 20, 16, 15],
-    "com": [18, 16, 13, 16, 15],
-    "audiovisuel": [12, 13, 11, 16, 10]
-}
+let m = [
+    [2,3,5],
+    [5,6,7],
+    [1,4,8]
+];
 
-function buildCard(etudiant) {
-    let div = document.createElement("div");
-    const {prenom, nom, age, web, com, audiovisuel} = etudiant
-    div.append(`${prenom} ${nom} (${age})`)
-    const ul =document.createElement('ul')
-    const createLi = () => document.createElement('li')
-    const li = createLi()
-    const li2 = createLi()
-    const li3 = createLi()
-    li.append(web)
-    li2.append(com)
-    li3.append(audiovisuel)
-    ul.append(li, li2, li3)
-    div.append(ul)
-    return div;
-}
+let t = m.flat();
+console.log(t)
+//  2, 3, 5, 5, 6,
+//  7, 1, 4, 8
 
-let etudiants = [etudiant1, etudiant2];
-for (let etudiant of etudiants) {
-    let d = buildCard(etudiant)
-    document.getElementById('root').append(d);
-}
+w = t.find(function (v) {
+    return v % 2 == 0;
+});
 
-let b = document.querySelector('button');
-b.onclick = () => {
-    let divs = document.querySelectorAll("#root div")
-    for (let div of divs)
-        div.classList.toggle("hidden");
-}
+console.log(w)
